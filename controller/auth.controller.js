0116744dcd4557d067c166b569a0cea6ab9f3727
Pooja <poojasri.s@skematix.in>
2026-02-20 10:09:50 +0530

    updates

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -75,6 +75,7 @@ async function forgotPassword(req, res) {
     return res.status(200).json({
       success: true,
       message: "Password reset email sent successfully",
+      token : token
     });
   } catch (error) {
     return res.status(500).json({
